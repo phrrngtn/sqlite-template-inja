443227f776ba8e41b63f004b15a797b45fee40b5
Paul Harrington <phrrngtn@panix.com>
2022-09-26 13:19:31 -0400

    improve the error-handling a bit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # sqlite-template-inja
 SQLite extension for templating with Inja
 
-Very rough initial revision without error-checking nor 
+Very rough initial revision without error-checking nor
 exception-handling.
 
 Developed on WSL but should run on anything with a modern C++
@@ -23,20 +23,27 @@ The project needs sqlite3ext.h and inja.hpp
   ```
 
 * load
-```
+```sql
 sqlite> .load ./template
 ```
-* extension exports just one function, `template_render` which takes an Inja template string and a JSON object and returns the 
+* extension exports just one function, `template_render` which takes an Inja template string and a JSON object and returns the
 template rendered wrt the JSON data.
-```
+```sql
 sqlite> select template_render('what is a  {{fruit}}, if not perfection', json_object('fruit', 'banana'));
 what is a  banana, if not perfection
 ```
-  
-Any errors at all (!) will cause the process to abort
-```
-sqlite> select template_render('what is a  {{fsdfuit}}, if not perfection', json_object('fruit', 'banana'));
-terminate called after throwing an instance of 'inja::RenderError'
-  what():  [inja.exception.render_error] (at 1:14) variable 'fsdfuit' not found
-Aborted
+
+Error handling is still a bit rough
+```sql
+-- bogus JSON (passed in a string that looks like JSON but isn't)
+sqlite> select template_render('what is a  {{}}, if not perfection', '{"fruitdsf", "banana"}');
+Runtime error: [json.exception.parse_error.101] parse error at line 1, column 12: syntax error while parsing object separator - unexpected ','; expected ':'
+
+-- fix the JSON (as a string literal) but with an invalid template
+sqlite> select template_render('what is a  {{}}, if not perfection', '{"fruitdsf": "banana"}');
+Runtime error: Inja exception: empty expression(render_error)
+
+-- valid template structure but wrong key in the JSON
+sqlite> select template_render('what is a  {{fruit}}, if not perfection', JSON_OBJECT("fruitdsf", "banana"));
+Runtime error: Inja exception: variable 'fruit' not found(render_error)
 ```
--- sqlite_template_inja.cpp
+++ sqlite_template_inja.cpp
@@ -18,6 +18,9 @@ extern "C"
 }
 
 // this is the 'logic' of the extension and can be written in idiomatic C++
+// exception handling etc can be done outside.
+// TODO: maybe pass in an Environment? Keep an LRU of templates?
+// It is probably expensive to parse the template for every function invocation
 std::string template_render(string template_string, string json_data)
 {
   json data = json::parse(json_data);
@@ -32,31 +35,46 @@ static void inja_func(
     int argc,
     sqlite3_value **argv)
 {
+  // TODO: replace with an error
   assert(argc == 2);
+
+  // do some more soundess checking
   if (sqlite3_value_type(argv[0]) == SQLITE_NULL)
     return;
 
   std::string expanded, template_string, json_data;
 
+  // hope that overloaded assignment operator will do the right thing.
+  // see if there is excessive copying going on.
   template_string = (reinterpret_cast<const char *>(sqlite3_value_text(argv[0])));
   json_data = (reinterpret_cast<const char *>(sqlite3_value_text(argv[1])));
+  // TODO: figure out when to return an error vs sqlite3_result_null
   try
   {
     expanded = template_render(template_string, json_data);
   }
-  catch (inja::RenderError e)
+  catch (inja::InjaError  e)
   {
     // how to do sprintf-like formatting?
     std::string message = "Inja exception: " + e.message + "(" + e.type + ")";
     // TODO: figure out how the memory management is working so as to avoid memory-leaks
+    // "The sqlite3_result_error() and sqlite3_result_error16() routines make a
+    //  private copy of the error message text before they return"
     sqlite3_result_error(context, message.data(), message.length());
     return;
   }
+  catch (json::exception e) {
+    std::string message = e.what();
+    sqlite3_result_error(context, message.data(),message.length());
+    return;
+  }
 
-  // How do we catch exceptions or check for errors
-  // void sqlite3_result_error(sqlite3_context*, const char*, int);
+  // TODO: deal with encodings, preferred encodings etc.
   sqlite3_result_text(context, expanded.data(), expanded.length(), SQLITE_TRANSIENT);
   // not sure if we have to do anything with freeing 'expanded'
+  // I think it will be taken care of by the runtime simply by going out of scope
+  // and that nothing has to be done to it explicitly.
+  return;
 }
 
 int sqlite3_template_init(
