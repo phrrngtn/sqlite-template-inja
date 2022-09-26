d3c87a920f2060f83cb0e1be3a76836d635ee489
Paul Harrington <phrrngtn@panix.com>
2022-09-26 10:34:11 -0400

    initial revision with no error processing or exception handling

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # sqlite-template-inja
 SQLite extension for templating with Inja
+
+Very rough initial revision without error-checking nor 
+exception-handling.
+
+Developed on WSL but should run on anything with a modern C++
+compiler due to the excellent portability of sqlite, inja and nlohmann::json.
+
+Building
+========
+The project needs sqlite3ext.h and inja.hpp
+
+* build/install https://github.com/pantor/inja
+
+* compile extension
+
+  ```
+  g++ -I ../sqlite-amalgamation-3390200 \
+     -I/usr/local/include/inja \
+     -fpic -shared \
+     sqlite_template_inja.cpp -o template.so
+  ```
+
+* load
+```
+sqlite> .load ./template
+```
+* extension exports just one function, `template_render` which takes an Inja template string and a JSON object and returns the 
+template rendered wrt the JSON data.
+```
+sqlite> select template_render('what is a  {{fruit}}, if not perfection', json_object('fruit', 'banana'));
+what is a  banana, if not perfection
+```
+  
+Any errors at all (!) will cause the process to abort
+```
+sqlite> select template_render('what is a  {{fsdfuit}}, if not perfection', json_object('fruit', 'banana'));
+terminate called after throwing an instance of 'inja::RenderError'
+  what():  [inja.exception.render_error] (at 1:14) variable 'fsdfuit' not found
+Aborted
+```
---
+++ sqlite_template_inja.cpp
@@ -0,0 +1,63 @@
+
+#include <sqlite3ext.h>
+SQLITE_EXTENSION_INIT1
+
+#include <inja.hpp>
+#include <iostream>
+
+using json = nlohmann::json;
+using namespace std;
+
+extern "C" {
+  // This needs to be callable from C
+  int sqlite3_template_init(
+		  sqlite3 *db, 
+		  char **pzErrMsg, 
+		  const sqlite3_api_routines *pApi
+		);
+}
+
+// this is the 'logic' of the extension and can be written in idiomatic C++
+std::string template_render(string template_string, string json_data) {
+  json data = json::parse(json_data);
+ 
+  return inja::render(template_string, data); 
+}
+
+
+// This is the function that will be called from SQLite and it has to
+// take care of a lot of the details of type-conversion, error-checking etc.
+static void inja_func(
+    sqlite3_context *context,
+    int argc,
+    sqlite3_value **argv
+){
+   assert( argc==2);
+   if( sqlite3_value_type(argv[0])==SQLITE_NULL ) return;
+
+   std::string expanded, template_string, json_data;
+
+   template_string = (reinterpret_cast<const char*>(sqlite3_value_text(argv[0])));
+   json_data = (reinterpret_cast<const char*>(sqlite3_value_text(argv[1])));
+
+   expanded = template_render(template_string, json_data);
+
+   // How do we catch exceptions or check for errors
+   // void sqlite3_result_error(sqlite3_context*, const char*, int);
+   sqlite3_result_text(context, expanded.data(), expanded.length(), SQLITE_TRANSIENT);
+   // not sure if we have to do anything with freeing 'expanded'
+}
+
+int sqlite3_template_init(
+  sqlite3 *db, 
+  char **pzErrMsg, 
+  const sqlite3_api_routines *pApi
+){
+  int rc = SQLITE_OK;
+  SQLITE_EXTENSION_INIT2(pApi);
+  (void)pzErrMsg;  /* Unused parameter */
+  rc = sqlite3_create_function(db, "template_render", 2,
+                   SQLITE_UTF8|SQLITE_DETERMINISTIC,
+                   0, inja_func, 0, 0);
+  return rc;
+}
